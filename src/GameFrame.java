68661d2efce2cc7ed1548b744f8106f7b3188766
lucky634634 <khoa21102003@gmail.com>
2025-03-25 18:37:12 +0700

    Update

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -3,9 +3,12 @@ import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
     public GameFrame() {
+        add(new GamePanel());
+        pack();
+        setLocationRelativeTo(null);
         setTitle("Zace game");
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(600, 600);
+        setResizable(false);
     }
 }
